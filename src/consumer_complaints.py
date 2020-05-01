3f7380cd1b48f3159c129b52eeb7084f23e41602
Ching Hong Wu <chw@bu.edu>
2020-05-01 11:37:20 -0400

    Add files via upload

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -8,22 +8,23 @@ import csv
 import itertools
 import sys
 
-#local path for testing
-#input_path = "C:/Users/Ching/Documents/complaints.csv"
-#output_path = "C:/Users/Ching/Documents/report.csv "
 
-#Github directory path for testing 
+#-------------------------------------------------------------------------------------------------
+####Get and prepare data for processing####
+
+#Initializes input and out file path 
 try:
     input_path = sys.argv[1]
 except:
     input_path = "../input/complaints.csv"
-
 try:
     output_path = sys.argv[2]
 except:
     output_path = "../output/report.csv"
 
 
+#Read input file specified by input path
+#Create a two dimensional list storing the data in input file
 with open(input_path, 'r', newline='', encoding = 'utf-8') as csvfile:
     data_obj = csv.reader(csvfile, delimiter=',',quotechar='"')
     data = []
@@ -31,10 +32,12 @@ with open(input_path, 'r', newline='', encoding = 'utf-8') as csvfile:
         data.append(row)
     data = data[1:]
 
+
 #sort data according to accending order of time and alphabetic order of product name
 data.sort(key=lambda x: (x[0], x[1]))
 
-#Get lists of unique product and unique years
+
+#Get lists of unique product, year, company
 product = [i[1] for i in data]
 product_unique = list(set(product))
 year = [i[0].split('-')[0] for i in data]
@@ -45,56 +48,65 @@ company_unique = list(set(company))
 
 
 
-#Method 3s
-report3 = []
-dataSubset = []
-CompanyList = []
-n = 0
+#---------------------------------------------------------------------------------------------------
+####Process data to create output####
 
+#initialize report3 as an empty 2-d list for storing processed analytics
+#each row represents 1 specific product x year pair 
+report3 = []
 for i in itertools.product(product_unique, year_unique):
     report3.append(i)
 
+#Create data subsets grouped by product x year pair
+#all data in the same group will have same product x year
+dataSubset = []
 for i in range(len(report3)):
     dataSubset.append([])
 
-#subset data based on year product pair
+#allocate each data row/record to data subset with matching produc x year pair
 for z in data:
     for i in range(len(report3)):
         if z[1]==report3[i][0] and z[0].split('-')[0]==report3[i][1]:
             dataSubset[i].append(z)
 
-for i in range(len(report3)):        
+#Process data in data subset to obtain desired outputs
+for i in range(len(report3)):          
+    #initializes outputs and list to store company
     TotalComplaint = 0
     TotalNumCompanies = 0
-    CompanyList = [] #company list (include repetitions) for product x year pair
-    
-    #all possible companies 
+    CompanyList = [] 
+    #Initialize a dictionary to store number of complaints for individual companies
+    #complaints are intialized as 0
     CompDict = {}    
     for m in range(len(company_unique)):
         CompDict[company_unique[m]] = 0 
     
-    #get total number of complaint of each subset (each set represent data for product x year pair)
+    #Get total number of complaint of each product x pair/each data subset
     TotalComplaints = len(dataSubset[i])
     
-    #get non-unique list of companies for product x year, and count complaints per company
+    #get a comprehensive non-unique list of companies for each product x year pair/each data subset
+    #count and store complaints for each company
     for ii in dataSubset[i]:
         CompanyList.append(ii[7])
         CompDict[ii[7]] = CompDict[ii[7]]+1
-    
-    #get the total number of company from unique list of company 
+    #get a unique list of companies; count total number of companies in the list for each product x year pair
     TotalNumCompanies = len(list(set(CompanyList)))
     
     #sort company based on number of complaints 
     CompDict_S3 = sorted(CompDict.items(), key=lambda x: x[1], reverse=True)   
-    
     #get top percentage of complaint out of all companies for product x year pair
     if TotalComplaints != 0:
         TopPercent = round(CompDict_S3[0][1]/TotalComplaints*100)
     else:
         TopPercent = 0
     
-    #add all number to report 
+    #add total number of complaints, total number of companies, top percentage of complaint to report 
     report3[i] = report3[i] + (TotalComplaints,TotalNumCompanies,TopPercent)
+
+
+
+#-------------------------------------------------------------------------------------------------------
+#Format output data and write to csv file
 
 #delete rows in report if there is no complaints filed
 j = 0
@@ -115,11 +127,7 @@ for row in report_output:
     row[1] = int(row[1])
     row[0] = row[0].lower()
 
-
-
-
-
-#upload report as a csv file to GitHub top most output directory 
+#create a csv file in GitHub top most output directory; write output report to this csv
 with open(output_path, 'w', newline='') as csvfile:
     output = csv.writer(csvfile, delimiter=',', quotechar='"')
     output.writerows(report_output)   
